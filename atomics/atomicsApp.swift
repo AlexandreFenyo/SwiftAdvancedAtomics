3ca7a6a3ae32bba14176d92fb77b1cc46d9465a3
Alexandre Fenyo <github@fenyo.net>
2024-02-21 11:24:46 +0100

    Initial Commit

---
+++ atomics/atomicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  atomicsApp.swift
+//  atomics
+//
+//  Created by Alexandre Fenyo on 21/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct atomicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
