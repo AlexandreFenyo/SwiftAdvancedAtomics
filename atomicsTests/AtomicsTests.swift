a785bd42dbf31c64b68b47240ff89d97fed9afaa
Alexandre Fenyo <alexandre.fenyo@gmail.com>
2024-03-28 22:41:47 +0100

    add tests

--- atomicsTests/AtomicsTests.swift
+++ atomicsTests/AtomicsTests.swift
@@ -6,17 +6,21 @@
 //
 
 import XCTest
+@testable import atomics
 
-final class atomicsTests: XCTestCase {
-
+final class AtomicsTests: XCTestCase {
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        SpinLock.initialize()
+        
     }
-
+    
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        SpinLock.deinitialize()
     }
-
+    
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
@@ -24,12 +28,41 @@ final class atomicsTests: XCTestCase {
         // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
         // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
     }
-
+    
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
         measure {
             // Put the code you want to measure the time of here.
         }
     }
+    
+    func testBasic() {
+        let at_foo = AdvancedAtomic(Foo())
+        
+        // incrémenter foo.bar
+        at_foo.tryAtomic { foo in
+            foo.bar += 1
+        }
+        
+        // prendre une copie de foo.bar
+        let bar = at_foo.atomic { $0.bar }
+        print("bar:\(bar)")
+    }
 
+    func testBasicException() {
+        let at_foo = AdvancedAtomic(Foo())
+        
+        // incrémenter foo.bar
+        do {
+            try at_foo.tryAtomicEx { foo in
+                foo.bar += 1
+            }
+            
+            // prendre une copie de foo.bar
+            let bar = try at_foo.tryAtomicEx { $0.bar }
+            print("bar:\(bar)")
+        } catch {
+            print("Exception thrown")
+        }
+    }
 }
